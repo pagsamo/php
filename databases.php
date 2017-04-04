7ab50368646b5ca7b9c506d140b7c933fd082cfa
Christian Mark Bituin <bituin.chrismark@gmail.com>
2017-04-04 14:58:52 +0800

    add

--- databases.php
+++ databases.php
@@ -14,7 +14,37 @@ $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
  		" ( ".mysqli_connect_errno() . ")");
  }
 
+// Perform database query
+$menu_name = "Edit me";
+$position = 4;
+$visible = 1;
 
+
+$query2 = "INSERT INTO subjects (";
+$query2 .= " menu_name, position, visible";
+$query2 .= ") VALUES (";
+$query2 .= "'{$menu_name}', {$position}, {$visible}";
+$query2 .= ")";
+
+
+
+$query = "SELECT * FROM subjects";
+$result = mysqli_query($connection, $query2);
+if(!$result)
+{
+	die("Database query failed.");
+}
+
+
+// 3 return data if any
+// while($row = mysqli_fetch_row($result))
+// {
+// 	var_dump($row);
+// 	echo "<hr/>";
+// }
+
+// 4 release the data
+// mysqli_free_result($result);
 
  // 5. close database connection
- 
+ mysqli_close($connection);
